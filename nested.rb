3adc352ec2eaedfd252a8a9c4df9dc43e8213b4b
Indu Ramesh <induramesh@gmail.com>
2019-06-23 21:29:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-      }
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
  		  
         :grace_hopper => {
           :known_for => "COBOL",
@@ -87,7 +87,7 @@ def adding_matz
         }
      }
 
-    programmer_hash
+    #programmer_hash
 end
 
 def changing_alan
