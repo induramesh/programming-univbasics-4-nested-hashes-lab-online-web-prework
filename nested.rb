1389a478d18bf232c5b72b15bfc3dfad5639406d
Indu Ramesh <induramesh@gmail.com>
2019-06-23 21:26:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -67,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
